5a2a475a0602faca4ec89142ef61a79b9cbd3d7e
Emil <emil.hasslof@gmail.com>
2023-06-14 14:33:45 +0200

    deleted react-stuff

--- inhouse-bot-react/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import LeagueStatsTable from './LeagueStatsTable';
-
-createRoot(document.getElementById('app')).render(<LeagueStatsTable />);
