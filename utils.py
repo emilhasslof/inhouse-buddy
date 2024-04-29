6ef91132ab1b4c741c421e423ab8cfdc7dd59301
emilhasslof <emil.hasslof@gmail.com>
2024-04-29 02:39:03 +0200

    bugfixed and tested utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,55 @@
 from os import path
 import json
 
+
 snuslan_id="123169301094989825"
 wew_id="745215730592645221"
 guild_id = snuslan_id
+
+
+
+def add_win(*, guild_id, players, stats):
+    for player in players:
+        stats[player]["wins"] += 1
+        stats[player]["matches"] += 1
+
+def subtract_win(*, guild_id, players, stats):
+    for player in players:
+        stats[player]["wins"] = max(stats[player]["wins"] - 1, 0)
+        stats[player]["matches"] -= 1
+
+def add_loss(*, guild_id, players, stats):
+    for player in players:
+        stats[player]["losses"] += 1
+        stats[player]["matches"] += 1
+
+def subtract_loss(*, guild_id, players, stats):
+    for player in players:
+        stats[player]["losses"] = max(stats[player]["losses"] - 1, 0)
+        stats[player]["matches"] -= 1
+
+
+def calculate_stats(stats):
+    players_ranked = sorted(stats.items(), key=lambda x: x[1]["points"], reverse=True)
+    prev_points = None
+    prev_rank = None
+    for i, (player, _) in enumerate(players_ranked, start=1):
+        if stats[player]["points"] == prev_points:
+            stats[player]["rank"] = prev_rank
+        else: 
+            stats[player]["rank"] = i
+
+        prev_points = stats[player]["points"]
+        prev_rank = stats[player]["rank"]
+
+        if stats[player]["matches"] == 0:
+            stats[player]["winrate"] = 0
+        else:
+            stats[player]["winrate"] = round(stats[player]["wins"] / stats[player]["matches"], 2)
+
+
+        stats[player]["points"] = stats[player]["wins"] - stats[player]["losses"]
+
 
 if path.exists(f"{guild_id}.json"):
     stats = {}
@@ -19,64 +65,20 @@ if path.exists(f"{guild_id}.json"):
         dire = ["kingo.1337", ".skiipa", "sku6808", "lackosia", "jointzart"] 
 
         for player in radiant + dire:
-            print(player)
+            if player not in stats: 
+                print(f"warning, {player} was not found in stats")
+                stats[player] = {"wins": 0, "losses": 0, "matches": 0, "winrate": 0, "points": 0, "rank": 0}
 
-        #update_ranks(stats)
+        add_win(guild_id=guild_id, players=radiant, stats=stats)
+        subtract_loss(guild_id=guild_id, players=radiant, stats=stats)
+
+        add_loss(guild_id=guild_id, players=dire, stats=stats)
+        subtract_win(guild_id=guild_id, players=dire, stats=stats)
+
+        # update score, winrate, rank
+        calculate_stats(stats)
 
     # Overwrite json with new stats
     with open(f"{guild_id}.json", "w") as file:
         stats_json = json.dumps(stats, indent=4)
         file.writelines(stats_json)
-
-
-def add_win(*, guild_id, players, stats):
-    if player not in stats:
-        print(f"warning, {player} was not found in stats")
-        return
-    for player in players:
-        stats[player]["wins"] += 1
-        stats[player]["points"] += 1
-        stats[player]["matches"] += 1
-        stats[player]["winrate"] = round(stats[player]["wins"] / stats[player]["matches"], 2)
-
-def subtract_win(*, guild_id, players):
-    if player not in stats:
-        print(f"warning, {player} was not found in stats")
-        return
-    for player in players:
-        stats[player]["wins"] -= 1
-        stats[player]["points"] -= 1
-        stats[player]["matches"] -= 1
-        stats[player]["winrate"] = round(stats[player]["wins"] / stats[player]["matches"], 2)
-
-def add_loss(*, guild_id, players):
-    for player in players:
-        stats[player]["losses"] += 1
-        stats[player]["points"] -= 1
-        stats[player]["matches"] += 1
-        stats[player]["winrate"] = round(stats[player]["wins"] / stats[player]["matches"], 2)
-
-def subtract_loss(*, guild_id, players):
-    if player not in stats:
-        print(f"warning, {player} was not found in stats")
-        return
-    for player in players:
-        stats[player]["losses"] -= 1
-        stats[player]["points"] += 1
-        stats[player]["matches"] -= 1
-        stats[player]["winrate"] = round(stats[player]["wins"] / stats[player]["matches"], 2)
-
-
-def update_ranks(stats):
-    # Sort players by points and update ranking
-    players_ranked = sorted(stats.items(), key=lambda x: x[1]["points"], reverse=True)
-    prev_points = None
-    prev_rank = None
-    for i, (player, _) in enumerate(players_ranked, start=1):
-        if stats[player]["points"] == prev_points:
-            stats[player]["rank"] = prev_rank
-        else: 
-            stats[player]["rank"] = i
-
-        prev_points = stats[player]["points"]
-        prev_rank = stats[player]["rank"]
