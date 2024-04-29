139b7534c6ec23aa3a12b7b45f28a38d756997f0
Emil Hasslöf <emil.hasslof@gmail.com>
2024-04-29 01:58:43 +0100

    added some template comments

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import json
 
 snuslan_id="123169301094989825"
 wew_id="745215730592645221"
-guild_id = snuslan_id
+guild_id = wew_id
 
 
 
@@ -59,21 +59,22 @@ if path.exists(f"{guild_id}.json"):
                 
 
         # add win, subtract loss
-        radiant = ["flyver123", "jakob7121", "cricket9584", "jockwe", "mandelmans"]
+        #radiant = ["slyver123", "jakob7121", "cricket9584", "jockwe", "mandelmans"]
+        radiant = []
 
         # subtract win, add loss
-        dire = ["kingo.1337", ".skiipa", "sku6808", "lackosia", "jointzart"] 
+        #dire = ["kingo.1337", ".skiipa", "sku6808", "lackosia", "jointzart"] 
+        dire = []
 
         for player in radiant + dire:
             if player not in stats: 
                 print(f"warning, {player} was not found in stats")
                 stats[player] = {"wins": 0, "losses": 0, "matches": 0, "winrate": 0, "points": 0, "rank": 0}
 
-        add_win(guild_id=guild_id, players=radiant, stats=stats)
-        subtract_loss(guild_id=guild_id, players=radiant, stats=stats)
+        players = []
+        #add_win(guild_id=guild_id, players=players, stats=stats)
+        #subtract_loss(guild_id=guild_id, players=players, stats=stats)
 
-        add_loss(guild_id=guild_id, players=dire, stats=stats)
-        subtract_win(guild_id=guild_id, players=dire, stats=stats)
 
         # update score, winrate, rank
         calculate_stats(stats)
